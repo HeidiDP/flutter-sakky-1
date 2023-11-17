130629baf8500043aacfae1d9bc1dbd9c3481b74
HeidiDP <heidi.damsten@gmail.com>
2023-11-17 23:52:46 +0200

    uusi_monivalinta

--- uusi_monivalinta/lib/question_screen.dart
+++ uusi_monivalinta/lib/question_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uusi_monivalinta/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -10,15 +11,23 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  var currrentQuestionIndex = 0;
+
   @override
   Widget build(context) {
+    final currentQuestion = questions[currentQuestionIndex]
 
     return Container(
       margin: const EdgeInsets.all(40),
       child: const Center(
         child: Column(
           children: [
-            Text('Heippa'),
+            Text(
+              currentQuestion.text,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox( height: 30,
+            )
           ],
         ),
       ),
