f7376d12eb771da516f9c12c4222cd23db617476
HeidiDP <heidi.damsten@gmail.com>
2023-11-20 19:48:56 +0200

    meals

--- meals/lib/screens/meals.dart
+++ meals/lib/screens/meals.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_details.dart';
+import 'package:meals/widgets/meal_item.dart';
+
 
 class MealsScreen extends StatelessWidget {   //1. tee luokka
   const MealsScreen({super.key, required this.title, required this.meals});             //2 tee constructori
@@ -7,14 +10,23 @@ class MealsScreen extends StatelessWidget {   //1. tee luokka
 final String title;         // tulee gategory griditem objetista
 final List<Meal> meals;     //tulee gategory griiditem objektista, tarvitaan lista meal objekteja
 
+void selectMeal(context, Meal meal){
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (ctx) => MealDetailsScreen(meal: meal),
+      ),
+  );
+}
   @override                                   //3. build tarvitsee overiden
   Widget build(BuildContext context){         //4. build widget 
 
     Widget content = ListView.builder(
       itemCount: meals.length,  //määrällisesti kaikki mitä löytyy ->itemcount, kerrotaan montako asiaa listaan on tulossa
       //(ctx, index) indeksi on laskuri joka lähtee 0,1,2,3,....
-      itemBuilder: (ctx, index) => Text(meals[index].title),   //()=> on nuolifunktio joka palauttaa widgetin (Text), tarvitsee 
-      //meals listasta haetaan indexin perusteella kaikki otsikot, ruokien nimet
+      itemBuilder: (ctx, index) => MealItem(meal:meals[index], onSelectMeal: () {
+        selectMeal(context, meals[index]);
+      },),   //()=> on nuolifunktio joka palauttaa widgetin (Text), tarvitsee 
+      //meals listasta haetaan indexin perusteella kaikki otsikot, ruokien nimet. funktio käy läpi kaikki listan ateriat ateria kerrallaan indexin mukaan
       );       
  
  //tarkistetaan onko lista tyhjä
--- meals/lib/widgets/meal_item.dart
+++ meals/lib/widgets/meal_item.dart
@@ -1,23 +1,107 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 //tämä rakentaa ateriakohtaisen widgetin
 
 class MealItem extends StatelessWidget {
-  const MealItem ({super.key, required this.meal});
+  const MealItem ({super.key, required this.meal, required this.onSelectMeal});
 
 final Meal meal;
+final void Function() onSelectMeal;
+
+String get complexityText{// 0 indeksi on aina iso kirjain ja loput sitten 1 indexizstä alkaen pieniä 
+  return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1);
+}
+String get affordabilityText{// 0 indeksi on aina iso kirjain ja loput sitten 1 indexizstä alkaen pieniä 
+  return meal.affordability.name[0].toUpperCase() + meal.affordability.name.substring(1);
+}
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+        ),
+        clipBehavior: Clip.hardEdge,
+        elevation: 2,
       child: InkWell(
-        onTap: (){},
+        //koodin jäsennys malli MVC(PHP, AsP.NET) = model, hallitaan tietokantaa (SQL)
+        // view = genereoidaan käyttöliittymä, controller=erilaisen datan hallintaan esim käytetään model luokkaa
+        onTap: onSelectMeal,
         child: Stack(
           //widgetit jotka ovat päällekkäin(huom, ylin on alin)
           children: [
             //kuva latautuu, ilmestyy hitaasti
-            FadeInImage(placeholder: placeholder, image: image)
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage), 
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
+              ),
+              Positioned(
+                //määritellään mistä boksi alkaa, tämä tulee kuvan päälle, kuva kuultaa alta läpi
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  color: Colors.black54,
+                  padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
+                  child:  Column(
+                    children: [
+                      Text(
+                        //eka rivi
+                        meal.title,
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                        softWrap: true,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          shadows: [BoxShadow(color: Colors.black,
+                          offset: Offset(2.0, 2.0,),
+                          blurRadius: 3.0,)]
+                        ),
+                        ),
+                     const SizedBox(
+                        height: 12,
+                      ),
+                       Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        //toka rivi
+                        children: [
+                          //ensimmäinen ikoni
+                          MealItemTrait(
+                            icon: Icons.schedule, 
+                            label:'${meal.duration} min',
+                            ),
+                            const SizedBox(
+                              width: 12,
+                            ),
+                            //toinen ikoni
+                            MealItemTrait(
+                            icon: Icons.work, 
+                            label: complexityText,
+                            ),
+                            const SizedBox(
+                              width: 12,
+                            ),
+                            //kolmas ikoni
+                            MealItemTrait(
+                            icon: Icons.attach_money, 
+                            label: affordabilityText,
+                            ),
+
+                        ],
+                      )],
+                  ),
+                ),
+                )
           ],
         ),
       ),
