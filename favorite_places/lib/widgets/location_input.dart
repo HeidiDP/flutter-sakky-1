cb6af49fafb7d34f9e7d4df74f6075c79a9473f1
HeidiDP <heidi.damsten@gmail.com>
2024-01-09 19:49:13 +0200

    favorite_places

--- favorite_places/lib/widgets/location_input.dart
+++ favorite_places/lib/widgets/location_input.dart
@@ -16,23 +16,23 @@ bool serviceEnabled;
 PermissionStatus permissionGranted;
 LocationData locationData;
 
-_serviceEnabled = await location.serviceEnabled();
-if (!_serviceEnabled) {
-  _serviceEnabled = await location.requestService();
-  if (!_serviceEnabled) {
+serviceEnabled = await location.serviceEnabled();
+if (!serviceEnabled) {
+  serviceEnabled = await location.requestService();
+  if (!serviceEnabled) {
     return;
   }
 }
 
-_permissionGranted = await location.hasPermission();
-if (_permissionGranted == PermissionStatus.denied) {
-  _permissionGranted = await location.requestPermission();
-  if (_permissionGranted != PermissionStatus.granted) {
+permissionGranted = await location.hasPermission();
+if (permissionGranted == PermissionStatus.denied) {
+  permissionGranted = await location.requestPermission();
+  if (permissionGranted != PermissionStatus.granted) {
     return;
   }
 }
 
-_locationData = await location.getLocation();
+locationData = await location.getLocation();
   }
   @override
   Widget build(BuildContext context) {
