e5243e6c12780138b033d9d8f459006d7d606824
HeidiDP <heidi.damsten@gmail.com>
2024-02-06 19:30:55 +0200

    chat

--- chat/lib/screens/auth.dart
+++ chat/lib/screens/auth.dart
@@ -8,6 +8,24 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+//onko kirjautuminen vai rekisteröinti
+var _isLogin = true; //false == luodaan uusi tili
+
+final _formKey = GlobalKey<FormState>();
+
+var _enteredEmail = '';
+var _enteredPassword = '';
+
+void _submit(){
+ final isValid = _formKey.currentState!.validate();
+ if(isValid){
+  _formKey.currentState!.save();
+  print(_enteredEmail);
+  print(_enteredPassword);
+
+ }
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +40,9 @@ class _AuthScreenState extends State<AuthScreen> {
                 margin: const EdgeInsets.all(20),
                 child: Padding(
                   padding: const EdgeInsets.all(16),
-                  child: Form(child: Column(
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       TextFormField(
@@ -31,12 +51,50 @@ class _AuthScreenState extends State<AuthScreen> {
                         keyboardType: TextInputType.emailAddress,
                         autocorrect: false,
                         textCapitalization: TextCapitalization.none,
+                        validator: (value){
+                          if(value == null || value.trim().isEmpty || !value.contains('@')) {
+                            return 'Please enter a valid email address!';
+                          }
+                          return null;
+                        },
+                        onSaved: (newValue){
+                          _enteredEmail = newValue!;
+                        },
                         ),
                         TextFormField(
                         decoration: 
                           const InputDecoration(labelText: 'Password'),
                         obscureText: true,
+                        validator: (value){
+                          if(value == null || value.trim().length < 6) {
+                            return 'Password must e at least 6 characters long!';
+                          }
+                          return null;                       
+                        },
+                        onSaved: (newValue){
+                          _enteredPassword = newValue!;
+                        },
                         ),
+                        const SizedBox(height: 12,
+                        ),
+                        ElevatedButton(
+                          onPressed: _submit, 
+                          child: Text(_isLogin 
+                          ? 'Login'
+                          :'Signup',
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: (){
+                            setState(() {
+                              _isLogin = !_isLogin;
+                            });
+                          }, 
+                          child: Text(
+                            _isLogin 
+                            ?'Create an Account' 
+                            : 'I already have an account'),
+                          )
                         ],
                       ),
                     ),
